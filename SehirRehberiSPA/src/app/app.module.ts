4949e5d37a979c1dba8444cce70125907888a3cf
Ahmet Soğuksu <dzcahmet@gmail.com>
2021-09-11 18:48:41 +0300

    v2

--- SehirRehberiSPA/src/app/app.module.ts
+++ SehirRehberiSPA/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { CityComponent } from './city/city.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
+  declarations: [AppComponent, ValueComponent, NavComponent, CityComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
